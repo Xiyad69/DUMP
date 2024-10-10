36a4664d4d89c158a9ec49a66d40378e114526c5
XIYAD <118036348+Xiyad69@users.noreply.github.com>
2024-10-11 03:03:21 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,14 +1,1 @@
-#python3.11
-#!/usr/bin/python
-#Riyad Mahfuz 
-#coded by XIYAD
-#Don’t copy
-#Don't Copy This Script
-
-import os, sys, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-    
 import file
